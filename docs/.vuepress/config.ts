0b83358697cbcad6910f6b293b46d4066626588d
Zyynow <128598666+Zyynow@users.noreply.github.com>
2023-08-18 04:01:38 +0000

    test

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,69 +2,80 @@ import { defaultTheme, defineUserConfig } from "vuepress";
 import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
 
 export default defineUserConfig({
-    base: "/hello-learn/", // github仓库名
-    title: 'Hello Learning',
-    description: 'Just a lifelong learning',
+  base: "/hello-learn/", // github仓库名
+  title: 'Hello Learning',
+  description: 'Just a lifelong learning',
 
-    plugins: [
-      backToTopPlugin(),
-    ],
+  plugins: [
+    backToTopPlugin(),
+  ],
 
 
-    theme: defaultTheme({
-        // Navigation bar
-        navbar: [
-          // NavbarItem
-          {
-            text: 'Foo',
-            link: '/foo/',
-          },
-          // NavbarGroup
-          {
-            text: 'Group',
-            children: ['/group/foo.md', '/group/bar.md'],
-          },
-          {
-            text: 'Languages',
-            ariaLabel: 'Language Menu',
-            children: [
-              { text: 'English', link: '/' },
-              { text: 'Chinese', link: '/' },
-              { text: 'Japanese', link: '/' },
-            ],
-          },
-          // 字符串 - 页面文件路径
-          '/bar/README.md',
+  theme: defaultTheme({
+    // Navigation bar
+    navbar: [
+      // NavbarItem
+      {
+        text: 'Foo',
+        link: '/foo/',
+      },
+      // NavbarGroup
+      {
+        text: 'Group',
+        children: ['/group/foo.md', '/group/bar.md'],
+      },
+      {
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        children: [
+          { text: 'English', link: '/' },
+          { text: 'Chinese', link: '/' },
+          { text: 'Japanese', link: '/' },
         ],
-        sidebar: [
+      },
+      // 字符串 - 页面文件路径
+      '/bar/README.md',
+    ],
+    sidebar: [
+      // SidebarItem
+      {
+        text: 'Foo',
+        link: '/',
+        children: [
           // SidebarItem
           {
-            text: 'Foo',
-            link: '/',
-            children: [
-              // SidebarItem
-              {
-                text: 'github',
-                link: 'https://github.com',
-              },
-            ],
-          },
-          {
-            text: 'Array',
-            link: '/ds/array.md',
-          },
-          {
-            text: 'Linked List',
-            link: '/ds/linked.md',
-          },
-          {
-            text: 'Queue',
-            link: '/ds/queue.md',
-          },
-          {
-            text: 'Stack',
-            link: '/ds/stack.md',
+            text: 'github',
+            link: 'https://github.com',
           },
         ],
-      }),
+      },
+      {
+        text: 'Voo',
+        link: '/',
+        children: [
+          // SidebarItem
+          {
+            text: 'github',
+            link: 'https://github.com',
+          },
+        ],
+      },
+      {
+        text: 'Array',
+        link: '/ds/array.md',
+      },
+      {
+        text: 'Linked List',
+        link: '/ds/linked.md',
+      },
+      {
+        text: 'Queue',
+        link: '/ds/queue.md',
+      },
+      {
+        text: 'Stack',
+        link: '/ds/stack.md',
+      },
+    ],
+  }),
 });
