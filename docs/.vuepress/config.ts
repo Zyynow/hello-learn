de827bfd580b69029a2419c1ff6202db1a174eea
Zyynow <128598666+Zyynow@users.noreply.github.com>
2023-08-18 04:05:34 +0000

    test2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -55,7 +55,7 @@ export default defineUserConfig({
         children: [
           // SidebarItem
           {
-            text: 'github',
+            text: 'git',
             link: 'https://github.com',
           },
         ],
