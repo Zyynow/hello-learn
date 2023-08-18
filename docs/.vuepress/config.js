3801b1eefdc6ba4a55024480684e9df9469f3de9
Zyynow <128598666+Zyynow@users.noreply.github.com>
2023-08-18 00:12:09 +0000

    Add Navbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,22 @@ import { defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
     base: "/hello-learn/", // github仓库名
-    title: 'Hello Learning'
+    title: 'Hello Learning',
+    description: 'Just a lifelong learning',
+    theme: defaultTheme({
+        navbar: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+            {
+              text: 'Languages',
+              ariaLabel: 'Language Menu',
+              items: [
+                { text: 'English', link: '/' },
+                { text: 'Chinese', link: '/' },
+                { text: 'Japanese', link: '/' }
+              ]
+            }
+          ],
+      }),
 });
